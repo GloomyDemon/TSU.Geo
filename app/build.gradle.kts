e7e0e0deee1c8d6ef1037844501db87d3e701b7e
GloomyDemon <marat.mustafin.06@gmail.com>
2026-03-23 01:27:28 +0700

    Naming fix and kotlin+composeBom+gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.tsugeo"
+    namespace = "university.hits.tsugeo"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -12,7 +12,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.tsugeo"
+        applicationId = "university.hits.tsugeo"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
