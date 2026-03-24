0dbb46d32d5bf1ca3c59a64222e7c294e5736432
yvga <murmururururnya@gmail.com>
2026-03-24 19:23:57 +0700

    Update MainActivity.kt

--- app/src/main/java/university/hits/tsugeo/MainActivity.kt
+++ app/src/main/java/university/hits/tsugeo/MainActivity.kt
@@ -5,12 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import university.hits.tsugeo.core.ui.components.ButtonIconWithText
 import university.hits.tsugeo.core.ui.theme.TSUGeoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +19,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TSUGeoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ButtonIconWithText(
+                    onClick = { /* WIP */ },
+                    icon = ImageVector.Companion.vectorResource(R.drawable.ic_launcher_foreground),
+                    text = "Текст",
+                    enabled = true,
+                    contentDescription = "Тест"
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TSUGeoTheme {
-        Greeting("Android")
     }
 }
