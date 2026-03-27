d79710549424ebf3a3084337cf399701ed56c460
GloomyDemon <marat.mustafin.06@gmail.com>
2026-03-27 11:07:01 +0700

    Little node interfaces implementations fix

--- app/src/main/java/university/hits/tsugeo/core/domain/interfaces/node/IMatrixNode.kt
+++ app/src/main/java/university/hits/tsugeo/core/domain/interfaces/node/IMatrixNode.kt
@@ -2,7 +2,7 @@ package university.hits.tsugeo.core.domain.interfaces.node
 
 import university.hits.tsugeo.core.domain.enums.Direction
 
-interface IMatrixNode: IConnectableNode {
+interface IMatrixNode: IBaseNode, IConnectableNode {
     val x: Int
     val y: Int
     val neighbourByDirection: Map<Direction, IBaseNode?>
